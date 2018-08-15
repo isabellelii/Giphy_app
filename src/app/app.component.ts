4694e8c8be4b6d84304172609f9b27e79a0137a4
Isabelle <isabellelidberg@live.se>
2018-08-15 12:28:28 +0200

    get data from giphy api toplist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Giphy app';
-}
+  private apiUrl = 'https://api.giphy.com/v1/gifs/trending?api_key=L3iR4JmiKlOmBfAVcg9lbH4Hgke1fDeo&limit=25&rating=G';
+  data: any = {};
 
+  constructor(private http: Http) {
+    console.log('hey api is working');
+    this.getAll();
+    this.getData();
+  }
+
+  getData() {
+    return this.http.get(this.apiUrl)
+    .map((res: Response) => res.json())
+  }
+
+  getAll() {
+    this.getData().subscribe(data => {
+      console.log(data);
+      this.data = data
+    })
+  }
+}
